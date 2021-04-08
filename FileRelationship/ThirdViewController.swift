4cccec4ec511947d820f5faeaebbfc81d7cd3be7
Naoki-Nomi <n.naoki.116@gmail.com>
2021-04-08 16:30:38 +0900

    Initial Commit

---
+++ FileRelationship/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  FileRelationship
+//
+//  Created by Naoki Nomi on 2021/04/08.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet weak var label3: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        label3.text = "3つ目の画面"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FileRelationship/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FileRelationship
+//
+//  Created by Naoki Nomi on 2021/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        label1.text = "1つ目の画面"
+    }
+
+
+}
+
